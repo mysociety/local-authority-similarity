1cdc04591812c9296f2085159d9d7220e426cfec
Alex Parsons <alex.parsons@mysociety.org>
2022-09-06 11:37:42 +0000

    Initial commit of data

--- src/local_authority_similarity/__main__.py
+++ src/local_authority_similarity/__main__.py
@@ -1,4 +1,32 @@
 import rich_click as click
+from data_common.management.run_notebook import run_notebook
+from pathlib import Path
+import rich
+
+
+def build_emissions():
+    rich.print("[green]Building distance matrix for emissions[/green]")
+    run_notebook(Path("notebooks", "emissions.ipynb"))
+
+
+def build_imd():
+    rich.print("[green]Building distance matrix for IMD[/green]")
+    run_notebook(Path("notebooks", "imd.ipynb"))
+
+
+def build_physical():
+    rich.print("[green]Building distance matrix for physical distance[/green]")
+    run_notebook(Path("notebooks", "physical.ipynb"))
+
+
+def build_ruc():
+    rich.print("[green]Building distance matrix for RUC[/green]")
+    run_notebook(Path("notebooks", "ruc.ipynb"))
+
+
+def build_composite():
+    rich.print("[green]Building composite distance matrix[/green]")
+    run_notebook(Path("notebooks", "composite.ipynb"))
 
 
 @click.group()
@@ -11,8 +39,12 @@ def main():
 
 
 @cli.command()
-def example():
-    print("This is an example function")
+def build():
+    build_emissions()
+    build_imd()
+    build_physical()
+    build_ruc()
+    build_composite()
 
 
 if __name__ == "__main__":
