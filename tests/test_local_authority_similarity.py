97d513ae045a099388e428249e9fe4e3adf63947
Alex Parsons <alex.parsons@mysociety.org>
2023-03-08 14:44:54 +0000

    Update to use 2023 councils

--- tests/test_local_authority_similarity.py
+++ tests/test_local_authority_similarity.py
@@ -1,7 +1,17 @@
 import local_authority_similarity
 
 import pytest
+from pathlib import Path
+import pandas as pd
 
 
-def test_true_is_true():
-    assert True is True
+def test_right_number_councils():
+    packages = Path("data", "packages")
+
+    # iterate through each folder
+    for folder in packages.iterdir():
+        df = pd.read_csv(folder / "distance_map.csv")
+        a_col = "local-authority-code_A"
+        assert (
+            len(df[a_col].unique()) == 393
+        ), f"Wrong number of councils in {folder.name}"
