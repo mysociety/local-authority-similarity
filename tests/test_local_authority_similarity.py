fa0865389fb25da0fb5f8f04460671f12f4ead55
Alex Parsons <alex.parsons@mysociety.org>
2024-02-02 20:40:43 +0000

    Update for north yorkshire

--- tests/test_local_authority_similarity.py
+++ tests/test_local_authority_similarity.py
@@ -13,5 +13,5 @@ def test_right_number_councils():
         df = pd.read_csv(folder / "distance_map.csv")
         a_col = "local-authority-code_A"
         assert (
-            len(df[a_col].unique()) == 393
+            len(df[a_col].unique()) == 394
         ), f"Wrong number of councils in {folder.name}"
